7684a9a52e81025c3d53b06c3e26e59aa769f1db
compfixengineer <compfixengineer@gmail.com>
2024-05-05 22:02:35 +0400

    Lesson02Hard

---
+++ script.js
@@ -0,0 +1,10 @@
+let num = 266219;
+let prodOfNumber = 1;
+let arr = Array.from(String(num), Number);
+console.log(arr);
+
+for (i = 0; i <= arr.length-1; i++) {
+    prodOfNumber *= arr[i];
+};
+prodOfNumber = prodOfNumber**3; 
+console.log(String(prodOfNumber).slice(0,2));
